d4081e68d9ea67b9bd84b99aa0504a26ae92b9b4
Derek Peacock <derek.peacock@yahoo.com>
2021-11-02 20:18:35 +0000

    added display names

--- ASP-MVC-Contoso/Models/Student.cs
+++ ASP-MVC-Contoso/Models/Student.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_MVC_Contoso.Models
 {
     public class Student
     {
         public int StudentID { get; set; }
+        
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        
+        
+        [DataType(DataType.Date), DisplayName("Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
+        
         // Relationships or navigation properties
         public virtual ICollection<Enrollment> Enrollments { get; set; }
 
